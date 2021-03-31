351ef2e6a189dcafde232ece336a9645a39aee7f
Cooper Hammond <kepoorh@gmail.com>
2021-03-30 20:05:11 -0600

    better bongo cat

--- config.h
+++ config.h
@@ -20,13 +20,13 @@
   #define OLED_DISPLAY_128X64
 #endif
 
-#ifdef RGBLIGHT_ENABLE
-  #define RGBLIGHT_ANIMATIONS
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 150
-#endif
+// #ifdef RGBLIGHT_ENABLE
+//   #define RGBLIGHT_ANIMATIONS
+//   #define RGBLIGHT_HUE_STEP 8
+//   #define RGBLIGHT_SAT_STEP 8
+//   #define RGBLIGHT_VAL_STEP 8
+//   #define RGBLIGHT_LIMIT_VAL 150
+// #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 // #define SPLIT_USB_DETECT
